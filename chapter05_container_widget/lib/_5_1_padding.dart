0b82d57d1c65f067652aa6a5cbf851cef4a5799d
wangzhichao <392337950@qq.com>
2020-06-16 09:29:50 +0800

    5.1 填充（Padding）

---
+++ chapter05_container_widget/lib/_5_1_padding.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// 学习 Padding 容器类组件
+// 1, 给子组件添加补白
+// 2, 属性就一个：final EdgeInsetsGeometry padding; EdgeInsetsGeometry 是一个抽象类，
+// 一般使用它的子类 EdgeInsets， EdgeInsets 定义了设置填充的便捷方法。
+class PaddingRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('填充（Padding）')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Text(
+            'Hello world',
+            style: TextStyle(backgroundColor: Colors.red),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Padding(
+            // 左边添加8像素的补白
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text('Hello world',
+                style: TextStyle(backgroundColor: Colors.blue)),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              'Hello world',
+              style: TextStyle(backgroundColor: Colors.yellow),
+            ),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              'Hello world',
+              style: TextStyle(backgroundColor: Colors.yellow),
+            ),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              'Hello world',
+              style: TextStyle(backgroundColor: Colors.lightBlue),
+            ),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 16, 16, 8),
+            child: Text(
+              'Hello world',
+              style: TextStyle(backgroundColor: Colors.lightBlue),
+            ),
+          ),
+          Divider(
+            height: 1.0,
+            indent: 0.0,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ chapter05_container_widget/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:chapter05containerwidget/_5_1_padding.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '第五章 容器类组件',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: '第五章 容器类组件'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return PaddingRoute();
+              }));
+            },
+            colorBrightness: Brightness.dark,
+            color: Colors.blue,
+            child: Text('5.1 填充（Padding）'),
+          ),
+        ],
+      ),
+    );
+  }
+}
