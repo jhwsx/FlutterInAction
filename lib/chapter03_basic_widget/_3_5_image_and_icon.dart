a9b210ecf361c871184920c20b09b447a64d890d
wangzhichao <wangzhichao@adups.com>
2020-12-04 11:42:40 +0800

    chapter5-5.7

--- lib/chapter03_basic_widget/_3_5_image_and_icon.dart
+++ lib/chapter03_basic_widget/_3_5_image_and_icon.dart
@@ -18,25 +18,25 @@ class ImageIconRoute extends StatelessWidget {
           children: <Widget>[
             Image(
               // 不指定宽高,图片会根据当前父容器的限制,尽量显示原始图片大小.
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
             ),
             Image(
               // 指定了宽度,另一个属性默认是按比例缩放
-              image: AssetImage("images/avatar.png"),
+              image: AssetImage("images/avatar.webp"),
               width: 100,
             ),
             Image(
               // 指定了高度,另一个属性默认是按比例缩放
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
               height: 50,
             ),
             Image(
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
               height: 50,
               width: 100,
             ),
             Image.asset(
-              "images/avatar.png",
+              "images/avatar.webp",
               width: 100,
             ),
             Image(
@@ -50,20 +50,20 @@ class ImageIconRoute extends StatelessWidget {
               width: 100,
             ),
             Image(
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
               width: 100,
               height: 100,
               color: Colors.red[100],
               colorBlendMode: BlendMode.lighten,
             ),
             Image(
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
               width: 100,
               color: Colors.blue,
               colorBlendMode: BlendMode.difference,
             ),
             Image(
-              image: AssetImage('images/avatar.png'),
+              image: AssetImage('images/avatar.webp'),
               width: 100,
               height: 200,
               repeat: ImageRepeat.repeatY,
@@ -118,19 +118,19 @@ class ImageIconRoute extends StatelessWidget {
             Column(
               children: [
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.fill, // 通过改变原图的宽高比来填充目标容器
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.fill, // 通过改变原图的宽高比来填充目标容器
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.contain,
@@ -138,19 +138,19 @@ class ImageIconRoute extends StatelessWidget {
                   // target box.这是图片的默认适应规则，图片会在保证图片本身长宽比不变的情况下缩放以适应当前显示空间，图片不会变形。
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.contain, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.contain, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.cover,
@@ -158,19 +158,19 @@ class ImageIconRoute extends StatelessWidget {
                   // 会按图片的长宽比放大后居中填满显示空间，图片不会变形，超出显示空间部分会被剪裁。
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.cover, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.cover, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.fitWidth,
@@ -179,19 +179,19 @@ class ImageIconRoute extends StatelessWidget {
                   // 图片的宽度会缩放到显示空间的宽度，高度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.fitWidth, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.fitWidth, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.fitHeight,
@@ -200,19 +200,19 @@ class ImageIconRoute extends StatelessWidget {
                   // 图片的高度会缩放到显示空间的高度，宽度会按比例缩放，然后居中显示，图片不会变形，超出显示空间部分会被剪裁。
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.fitHeight, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.fitHeight, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.none,
@@ -222,19 +222,19 @@ class ImageIconRoute extends StatelessWidget {
                   // 图片没有适应策略，会在显示空间内显示图片，如果图片比显示空间大，则显示空间只会显示图片中间部分。
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.none, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.none, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 50,
                   fit: BoxFit.scaleDown,
@@ -246,13 +246,13 @@ class ImageIconRoute extends StatelessWidget {
                   // is the same as `none`.
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 50,
                   height: 100,
                   fit: BoxFit.scaleDown, //
                 ),
                 Image(
-                  image: AssetImage('images/avatar.png'),
+                  image: AssetImage('images/avatar.webp'),
                   width: 100,
                   height: 100,
                   fit: BoxFit.scaleDown, //
--- lib/chapter05_container_widget/_5_7_clip.dart
+++ lib/chapter05_container_widget/_5_7_clip.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ClipRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget avatar = Image.asset("images/wzc_avatar.webp", width: 60);
     return Scaffold(
       appBar: AppBar(
         title: Text("剪裁（Clip）"),
@@ -10,10 +11,100 @@ class ClipRoute extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-
+            Text(
+              "原图: ",
+            ),
+            avatar,
+            Text(
+              "裁剪为圆形: ",
+            ),
+            ClipOval(
+              child: avatar,
+            ),
+            ClipOval(
+              child: Image.asset(
+                "images/book.webp",
+                width: 60,
+              ),
+            ),
+            Text(
+              "裁剪为圆角矩形: ",
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: avatar,
+            ),
+            Text(
+              "不使用 ClipRect 溢出部分仍然会显示：",
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  widthFactor: 0.5,
+                  child: avatar,
+                ),
+                Text(
+                  "willwaywang6",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            // 使用了 ClipRect 包裹 Align，解决了图片溢出部分显示的问题。
+            Text(
+              "使用 ClipRect 溢出部分不再会显示：",
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClipRect(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    widthFactor: 0.5,
+                    child: avatar,
+                  ),
+                ),
+                Text(
+                  "willwaywang6",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "裁剪左边从 10.0 开始，顶部从 15.0 开始，宽x高为 40x30 的区域：",
+            ),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: ClipRect(
+                child: avatar,
+                clipper: MyCustomClipper(),
+              ),
+            )
           ],
         ),
       ),
     );
+  }
+}
+
+class MyCustomClipper extends CustomClipper<Rect> {
+  // 获取裁剪区域
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(10.0, 15.0, 40.0, 30.0);
+  }
+
+  // 接口决定是否重新剪裁。如果在应用中，剪裁区域始终不会发生变化时应该返回false，这样就不会触发重新剪裁，
+  // 避免不必要的性能开销。如果剪裁区域会发生变化（比如在对剪裁区域执行一个动画），那么变化后应该返回true来重新执行剪裁。
+  @override
+  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) {
+    return false;
   }
 }
