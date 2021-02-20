474981f2ca3c649801630817310c532edfa99bac
39233 <392337950@qq.com>
2021-02-20 10:45:01 +0800

    update 7.2

--- lib/chapter07_functional_widget/_7_2_inherited_widget.dart
+++ lib/chapter07_functional_widget/_7_2_inherited_widget.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 /// When using the `of` method, the `context` must be a descendant of the
 /// [InheritedWidget], meaning it must be "below" the [InheritedWidget] in the
 /// tree.
+/// InheritedWidget 的作用是什么？
+/// 在应用的根 widget 中通过 InheritedWidget 共享了一个数据，那么我们便可以在任意子 widget
+/// 中来获取该共享的数据。
 class InheritedWidgetRoute extends StatefulWidget {
   @override
   _InheritedWidgetRouteState createState() => _InheritedWidgetRouteState();
@@ -16,6 +19,7 @@ class _InheritedWidgetRouteState extends State<InheritedWidgetRoute> {
   // 接着会调用 TestWidget 的 didChangeDependencies 方法，发生更新。
   _incrementCount() {
     setState(() {
+      // count;
       ++count;
     });
   }
@@ -34,10 +38,20 @@ class _InheritedWidgetRouteState extends State<InheritedWidgetRoute> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: TestWidget1(), // 子 widget 有依赖 SharedDataWidget。
-                  ),
+                  // 正确的写法：
+                  TestWidget(), // 子 widget 有依赖 SharedDataWidget。
+                  // 正确的写法:
+                  // Builder(builder: (BuildContext context) {
+                  //   return Text(
+                  //     SharedDataWidget.of(context).data.toString(),
+                  //     style: TextStyle(color: Colors.green),
+                  //   );
+                  // }),
+                  // 错误的用法：传入的 context 并不是 InheritedWidget 的子类。
+                  // Text(
+                  //   SharedDataWidget.of(context).data.toString(),
+                  //   style: TextStyle(color: Colors.red),
+                  // ),
                   RaisedButton(
                     child: Text('Increment'),
                     onPressed: _incrementCount,
@@ -45,23 +59,6 @@ class _InheritedWidgetRouteState extends State<InheritedWidgetRoute> {
                 ],
               ),
             ),
-            SharedDataWidget(
-              data: count,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: TestWidget2(), // 子 widget 有依赖 SharedDataWidget。
-                  ),
-                  RaisedButton(
-                    child: Text('Increment2'),
-                    onPressed: _incrementCount,
-                  ),
-                ],
-              ),
-            ),
-
           ],
         ),
       ),
@@ -70,16 +67,19 @@ class _InheritedWidgetRouteState extends State<InheritedWidgetRoute> {
 }
 
 class SharedDataWidget extends InheritedWidget {
-  SharedDataWidget({Key key, @required this.data, Widget child}) : super(key: key, child: child);
+  SharedDataWidget({Key key, @required this.data, Widget child})
+      : super(key: key, child: child);
   final int data; // 需要在子树中共享的数据，就是点击次数。
   /// 这个方法的作用是方便子树的 widget 获取共享数据，它会注册依赖关系。
   static SharedDataWidget of(BuildContext context) {
+    /// 此方法定义在 BuildContext 中，但是它的实现是在 Element 中。
     return context.dependOnInheritedWidgetOfExactType<SharedDataWidget>();
   }
 
-  /// 这个方法的作用是获取 SharedDataWidget 的数据，但在 SharedDataWidget 发生变化时
+  /// 这个方法的作用是获取 SharedDataWidget 的数据，但在 SharedDataWidget 数据发生变化时
   /// 不会调用子或孙 widget 的 didChangeDependencies() 方法。它不会注册依赖关系。
   static SharedDataWidget of2(BuildContext context) {
+    /// 此方法定义在 BuildContext 中，但是它的实现是在 Element 中。
     return context
         .getElementForInheritedWidgetOfExactType<SharedDataWidget>()
         .widget;
@@ -94,17 +94,19 @@ class SharedDataWidget extends InheritedWidget {
   }
 }
 
-class TestWidget1 extends StatefulWidget {
+class TestWidget extends StatefulWidget {
   @override
-  _TestWidget1State createState() => _TestWidget1State();
+  _TestWidgetState createState() => _TestWidgetState();
 }
 
-class _TestWidget1State extends State<TestWidget1> {
+class _TestWidgetState extends State<TestWidget> {
   @override
   Widget build(BuildContext context) {
+    print('_TestWidgetState build() called');
     // 如果不使用 InheritedWidget 中的共享数据，那么当点击按钮时，didChangeDependencies 不会回调。
-    // return Text("text");
-    return Text(SharedDataWidget.of(context).data.toString());
+    return Text("text");
+    // return Text(SharedDataWidget.of(context).data.toString());
+    // return Text(SharedDataWidget.of2(context).data.toString());
   }
 
   @override
@@ -112,26 +114,7 @@ class _TestWidget1State extends State<TestWidget1> {
     super.didChangeDependencies();
     // 父或祖先 widget 中的 InheritedWidget 改变（updateShouldNotify 返回 true）时
     // 会被调用。如果 build 方法中没有依赖 InheritedWidget，则此回调不会被调用。
-    print('_TestWidget1State didChangeDependencies() called, because dependencies changed.');
-  }
-}
-
-class TestWidget2 extends StatefulWidget {
-  @override
-  _TestWidget2State createState() => _TestWidget2State();
-}
-
-class _TestWidget2State extends State<TestWidget2> {
-  @override
-  Widget build(BuildContext context) {
-    return Text(SharedDataWidget.of2(context).data.toString());
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // 父或祖先 widget 中的 InheritedWidget 改变（updateShouldNotify 返回 true）时
-    // 会被调用。如果 build 方法中没有依赖 InheritedWidget，则此回调不会被调用。
-    print('_TestWidget2State didChangeDependencies() called, because dependencies changed.');
+    print(
+        '_TestWidgetState didChangeDependencies() called, because dependencies changed.');
   }
 }
